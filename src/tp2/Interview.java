f04f20773eff7ce905bd8ff869fadcaf4fe58b41
MatheoBenoit <matheo.benoit-paraschivoiu@polymtl.ca>
2021-09-29 21:21:47 -0400

    Mat: presque fini interview

--- src/tp2/Interview.java
+++ src/tp2/Interview.java
@@ -10,7 +10,13 @@ public final class Interview {
      * @return boolean based on if the entropy is similar
      */
     public static Double compareEntropies(String filename1, String filename2) throws IOException {
-        return null;
+        HashMap<Character, Integer> firstMap = getFrequencyHashTable(readFile(filename1));
+        HashMap<Character, Integer> secondMap = getFrequencyHashTable(readFile(filename2));
+
+        Double firstEntropy = calculateEntropy(firstMap);
+        Double secondEntropy = calculateEntropy(secondMap);
+
+        return  Math.abs(firstEntropy - secondEntropy);
     }
 
     /** TODO
@@ -19,22 +25,48 @@ public final class Interview {
      * @return the difference in frequencies of two HashMaps
      */
     public static Integer compareFrequencies(String filename1, String filename2) throws IOException{
-        return null;
+        HashMap<Character, Integer> firstMap = getFrequencyHashTable(readFile(filename1));
+        HashMap<Character, Integer> secondMap = getFrequencyHashTable(readFile(filename2));
+
+        Integer sum = 0;
+        for (int i = 0; i < Math.max(firstMap.size(), secondMap.size()); i++) {
+
+
+
+        }
+
+        return sum;
     }
 
     /** TODO
      * @return This function returns the entropy of the HashMap
      */
     public static Double calculateEntropy(HashMap<Character, Integer> map){
-        return null;
+        double entropy = 0.0;
+        double length = 0.0;
+
+        for (Integer data : map.values()) {
+            length += data;
+        }
+        for (Integer data : map.values()) {
+            entropy += ((data / length) * ( Math.log( 1.0 / (data / length )) / Math.log( 2 ))); //to have log with base of 2
+        }
+        return entropy;
     }
 
     /**
-     * This function reads a text file {filenamme} and returns the appended string of all lines
+     * This function reads a text file {filename} and returns the appended string of all lines
      * in the text file
      */
     public static String readFile(String filename) throws IOException {
-        return null;
+        String text = "";
+        BufferedReader br = new BufferedReader(new FileReader(filename));
+        String tmp;
+
+        while ((tmp = br.readLine()) != null) {
+            text += tmp;
+        }
+        return text;
     }
 
     /** TODO
@@ -42,10 +74,21 @@ public final class Interview {
      * of character frequencies
      */
     public static HashMap<Character, Integer> getFrequencyHashTable(String text) {
-        return null;
+        HashMap<Character, Integer> map = new HashMap<> (text.length());
+
+        for (int i = 0; i < text.length(); i++) {
+            char character = text.charAt(i);
+            Integer freq = map.get(character);
+            if (freq != null) {
+                map.put(character, ++freq);
+            } else {
+                map.put(character, 1);
+            }
+        }
+        return map;
     }
 
-    /** TODO
+    /**
      * This function takes a character as a parameter and returns if it is a letter in the alphabet
      */
     public static Boolean isAlphabetic(Character c){
